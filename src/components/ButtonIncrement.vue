5f2143e17b882eb33d17695d2f426b609c7a0fe6
Delano Oliveira <delanohelio@gmail.com>
2025-09-05 14:40:20 -0300

    Adicionando novo botão de incremento

---
+++ src/components/ButtonIncrement.vue
@@ -0,0 +1,11 @@
+<template>
+  <button @click="increment">{{ count }}</button>
+</template>
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+const increment = () => {
+  count.value++
+}
+</script>
